deff45cc84d3521b6a7175fc885ca21631c2ba9d
eleith <eleith@noreply.git.eleith.com>
2023-02-15 16:26:37 +0000

    [nvim] customize whichkey for less interuption

--- config/nvim/lua/plugins/whichkey.lua
+++ config/nvim/lua/plugins/whichkey.lua
@@ -1,8 +1,13 @@
 return {
-  "folke/which-key.nvim",
-  config = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-    require("which-key").setup { }
-  end,
+	"folke/which-key.nvim",
+	config = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 600
+		require("which-key").setup({
+			window = {
+				border = "single",
+				position = "bottom",
+			},
+		})
+	end,
 }
