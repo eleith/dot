f74bf435a831cbced8e82e289072d03bf38d5c88
eleith <eleith@noreply.git.eleith.com>
2023-03-22 04:50:50 +0000

    [fish] install fisher if not available

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -52,6 +52,6 @@ end
 if not functions -q fisher
     echo "Installing fisher for the first time..." >&2
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fisher
+    curl https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher update
 end
