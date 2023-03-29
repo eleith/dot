33881ebaeeddf15ccfbd24014a098e293f1b8aa8
eleith <eleith@noreply.git.eleith.com>
2023-03-29 04:37:54 +0000

    [fish] avoid fisher install if not interactive

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -49,7 +49,7 @@ if type -q fzf
     end
 end
 
-if not functions -q fisher
+if not functions -q fisher && status is-interactive
     echo "Installing fisher for the first time..." >&2
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
