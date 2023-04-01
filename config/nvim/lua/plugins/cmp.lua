aacccb2fd040db8d635be9afb0f76b6dc87f806f
eleith <eleith@noreply.git.eleith.com>
2023-04-01 23:41:52 +0000

    [nvim] fix background colors on floats

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -22,19 +22,16 @@ return {
 				-- REQUIRED - you must specify a snippet engine
 				expand = function(args)
 					vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-					-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 				end,
 			},
 			window = {
 				completion = {
-					border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-					winhighlight = "Normal:CmpNormal",
+					border = "single",
+					winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None",
 				},
 				documentation = {
-					border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-					winhighlight = "Normal:CmpNormal",
+					border = "single",
+					winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None",
 				},
 			},
 			mapping = cmp.mapping.preset.insert({
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -4,12 +4,10 @@ return {
 	priority = 1000,
 	config = function()
 		local gruvbox = require("gruvbox")
-		local palette = require("gruvbox.palette")
 
 		gruvbox.setup({
 			overrides = {
-				NormalFloat = { bg = palette.bg0 },
-				CmpNormal = { bg = palette.bg0 },
+				NormalFloat = { bg = "none" },
 			},
 		})
 
