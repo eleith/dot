d9b107d12e39d5f71f81d2dc745233f022e9a8a6
eleith <eleith@noreply.git.eleith.com>
2023-05-10 13:19:27 +0000

    [wezterm] remove terminal window decorations

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -22,5 +22,6 @@ config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 12.0
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
+config.window_decorations = "RESIZE"
 
 return config
