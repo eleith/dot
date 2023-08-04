37b60e7fa8c0b480d233f2affaf8af2f3e8046a9
eleith <online-gitea@eleith.com>
2023-08-04 21:26:14 +0000

    [fish] test pathname existence before using it

--- config/fish/functions/fish_title.fish
+++ config/fish/functions/fish_title.fish
@@ -1,6 +1,5 @@
 function fish_title --argument-names last_cmd
     set prefix ""
-    set last_path (string split ' ' $last_cmd)[2..]
 
     if set -q SSH_CLIENT
         if not set -q TMUX
@@ -13,7 +12,14 @@ function fish_title --argument-names last_cmd
     if [ fish != $_ ]
         switch $_
             case vi vim nvim
+              set last_path (string split ' ' $last_cmd)[2..]
+
+              if test -n "$last_path"
+                and test -f "$last_path"
                 echo "$prefix$_ $(path basename $last_path)"
+              else
+                echo "$prefix$last_cmd"
+              end 
             case '*'
                 echo "$prefix$last_cmd"
         end
