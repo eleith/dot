6e60b28ef8f20e1d76f30faf74e05b094dc6af34
eleith <eleith@noreply.git.eleith.com>
2024-08-25 00:42:08 +0000

    [nvim] simplify whichkey

--- config/nvim/lua/plugins/whichkey.lua
+++ config/nvim/lua/plugins/whichkey.lua
@@ -1,13 +1,18 @@
 return {
 	"folke/which-key.nvim",
-	config = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 600
-		require("which-key").setup({
-			window = {
-				border = "single",
-				position = "bottom",
-			},
-		})
-	end,
+	event = "VeryLazy",
+	opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
 }
