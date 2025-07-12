35d1030b33e3ce95de0149cc9218ded1a9aa7f3e
eleith <eleith@noreply.git.eleith.com>
2025-07-12 14:51:13 +0000

    [wezterm] use default fonts

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -18,8 +18,6 @@ local function scheme_for_appearance(appearance)
 end
 
 config.color_scheme = scheme_for_appearance(get_appearance())
-config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 12.0
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
 config.window_decorations = "RESIZE"
