3459b3fcb08345bd2d5a1c97719de70c58b5f6f9
eleith <eleith@noreply.git.eleith.com>
2026-02-04 23:34:14 +0000

    [nvim] simplify golanglint lsp config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -77,21 +77,7 @@ return {
 			},
 			{ "gopls" },
 			{ "templ" },
-			{
-				"golangci_lint_ls",
-				settings = {
-					cmd = { "golangci-lint-langserver" },
-					filetypes = { "go", "gomod" },
-					init_options = {
-						command = {
-							"golangci-lint", "run", "--output.json.path", "stdout", "--show-stats", "false", "--issues-exit-code=1"
-						}
-					},
-					root_markers = {
-						".golangci.yml", ".golangci.yaml", ".golangci.toml", ".golangci.json", "go.work", "go.mod", ".git"
-					}
-				}
-			},
+			{ "golangci_lint_ls" },
 			{ "eslint" },
 			{
 				"yamlls",
