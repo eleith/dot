dbd240431d286721857f81ecc7f9fcbd731b5123
eleith <1+eleith@noreply.git.eleith.com>
2026-09-09 19:58:50 +0000

    [wezterm] sync tmux themes, fix flatpak bell

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -17,6 +17,38 @@ local function scheme_for_appearance(appearance)
 	end
 end
 
+local function sync_tmux_theme(appearance)
+	local conf = (wezterm.home_dir .. "/.config/tmux/tmux.conf")
+
+	local has_tmux = wezterm.run_child_process({
+		"flatpak-spawn",
+		"--host",
+		"tmux",
+		"has-session"
+	})
+
+	-- If tmux isn't running, exit quietly without throwing an error
+	if not has_tmux then
+		return
+	end
+
+	local success, stdout, stderr = wezterm.run_child_process({
+		"flatpak-spawn",
+		"--host",
+		"tmux",
+		"source-file",
+		conf
+	})
+
+	if not success then
+		wezterm.log_error("Failed to sync tmux theme: " .. tostring(stderr))
+	end
+end
+
+wezterm.on("window-config-reloaded", function(window, pane)
+	sync_tmux_theme(window:get_appearance())
+end)
+
 config.color_scheme = scheme_for_appearance(get_appearance())
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
@@ -24,16 +56,28 @@ config.window_decorations = "RESIZE"
 config.warn_about_missing_glyphs = false
 
 -- disable audio bell
+-- config.audible_bell = "Disabled"
+config.audible_bell = "SystemBeep"
 
-config.audible_bell = "Disabled"
+-- 2. Hook into the bell event to play a sound manually
+wezterm.on('bell', function(window, pane)
+	-- Run the audio player in the background so it doesn't freeze the terminal
+	wezterm.background_child_process {
+		'flatpak-spawn',
+		'--host',
+		"pw-play",
+		"/usr/share/sounds/freedesktop/stereo/bell.oga" -- Replace with your tested path
+	}
+end)
+
 config.visual_bell = {
-  fade_in_function = 'EaseIn',
-  fade_in_duration_ms = 150,
-  fade_out_function = 'EaseOut',
-  fade_out_duration_ms = 150,
+	fade_in_function = 'EaseIn',
+	fade_in_duration_ms = 150,
+	fade_out_function = 'EaseOut',
+	fade_out_duration_ms = 150,
 }
 config.colors = {
-  visual_bell = '#202020',
+	visual_bell = '#202020',
 }
 
 -- disable update check
