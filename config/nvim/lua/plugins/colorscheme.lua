a4e038639f0ef52888b9dee7609d3d806fa73ac7
eleith <1+eleith@noreply.git.eleith.com>
2026-09-17 16:29:53 +0000

    [nvim] add modus theme plugin

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,36 @@
+-- return {
+-- 	"ellisonleao/gruvbox.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		local gruvbox = require("gruvbox")
+-- 
+-- 		gruvbox.setup({
+-- 			overrides = {
+-- 				NormalFloat = { bg = "none" },
+-- 			},
+-- 		})
+-- 
+-- 		vim.cmd.colorscheme("gruvbox")
+-- 	end,
+-- }
+
 return {
-	"ellisonleao/gruvbox.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		local gruvbox = require("gruvbox")
-
-		gruvbox.setup({
-			overrides = {
-				NormalFloat = { bg = "none" },
+  "miikanissi/modus-themes.nvim",
+  priority = 1000,
+  config = function()
+    require("modus-themes").setup({
+      style = "auto", -- auto switches based on vim.o.background
+      variants = {
+				modus_operandi = 'tinted',
+				modus_vivendi = 'tinted',
 			},
-		})
-
-		vim.cmd.colorscheme("gruvbox")
-	end,
+      transparent = false,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = false },
+      },
+    })
+    vim.cmd("colorscheme modus")
+  end,
 }
